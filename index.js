d0bfcdddd1d8b08bd8a4c9ec162b92defa853f10
Emmanuel Mahuni <emahuni@users.noreply.github.com>
2022-08-02 12:25:08 +0200

    feat(index): get paths to all actual circulars in obj
    Instead of getting just the first circular go further and get all circular objects.
    This modification targets the actual circular key to allow user to actually do something about it

--- index.js
+++ index.js
@@ -10,20 +10,23 @@ function whatIsCircular (obj) {
 }
 
 function _dfs (obj, parents = [], parentKeys = []) {
+  const circular = [];
   // recurse depth-first until we hit something we've seen before
   for (const key in obj) {
     const val = obj[key]
 
     if (isObject(val)) {
       if (includes(parents, val)) {
-        return parentKeys
+        circular.push(parentKeys.concat([key]))
+        continue
       }
 
       const path = _dfs(val, parents.concat([val]), parentKeys.concat([key]))
 
-      if (path) return path
+      if (path) circular.push (path)
     }
   }
+  if(circular) return circular
 }
 
 module.exports = whatIsCircular
